ab5baa08c6b3f175767077d2c7bbc382016d3957
Hassan A <has_is_ere@hotmail.com>
2018-07-23 13:05:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
 dodger.style.backgroundColor= '#FF69B4'
+
+
+document.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
+
