c2675db1b1eee4c453330230be36b0cd17b72acb
Hassan A <has_is_ere@hotmail.com>
2018-07-23 13:06:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ document.addEventListener('keydown', function(e) {
   console.log(e.which)
 })
 
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
