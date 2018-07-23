256cdf0f58ca55dd260f82795b65c7644aba7d0f
Hassan A <has_is_ere@hotmail.com>
2018-07-23 13:30:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function moveDodgerRight() {
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
-  } else if (e.which ===39){
+  } else if (e.which === 39){
     moveDodgerRight()
   }
 })
