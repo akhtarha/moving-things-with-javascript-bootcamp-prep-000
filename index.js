55060acfe2cf3af823bbac88b3115de51414768b
Hassan A <has_is_ere@hotmail.com>
2018-07-23 13:32:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function moveDodgerLeft() {
 
 function moveDodgerRight() {
   console.log(dodger.style.left)
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
+ var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
   console.log("right: "+right)
   if (right > 0) {
     dodger.style.right = `${right + 1}px`
