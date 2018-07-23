886f9190a26d77b89bfc792c749113c83b29b6ad
Hassan A <has_is_ere@hotmail.com>
2018-07-23 13:27:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,19 +17,17 @@ function moveDodgerLeft() {
   }
 }
 
-function moveDodgerRightt() {
+function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
+  
   var rightNumbers = dodger.style.right.replace('px', '')
   var right = parseInt(rightNumbers, 10)
   
-  if (left > 0) {
+  if (right > 0) {
     dodger.style.left = `${left - 1}px`
   }
 }
-
-
-
 
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
