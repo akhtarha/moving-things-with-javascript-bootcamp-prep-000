266fe12085cddc9d0d996f00906eabdd43e3259b
Hassan A <has_is_ere@hotmail.com>
2018-07-23 13:11:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ const app = "I don't do much."
 var dodger = document.getElementById('dodger')
 dodger.style.backgroundColor= '#FF69B4'
 
-
 document.addEventListener('keydown', function(e) {
   console.log(e.which)
 })
 
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
   var left = parseInt(leftNumbers, 10)
  
   if (left > 0) {
